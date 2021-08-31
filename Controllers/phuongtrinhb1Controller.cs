c5bad6bfc8d42d01c8511c4959f13715fd5fff63
quanga123 <quangoc0302@gmail.com>
2021-08-31 09:36:07 +0700

    quang

---
+++ Controllers/phuongtrinhb1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace phuongtrinhb1.Controllers
+{
+    public class phuongtrinhb1Controller : Controller
+    {
+        // GET: phuongtrinhb1
+        public ActionResult phuongtrinhb1()
+        {
+            return View();
+        }
+    }
+}
