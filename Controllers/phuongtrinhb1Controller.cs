f9eb253877c4d8e45ba9cf67090a2733920e7880
quanga123 <quangoc0302@gmail.com>
2021-09-07 08:37:39 +0700

    quang

--- Controllers/phuongtrinhb1Controller.cs
+++ Controllers/phuongtrinhb1Controller.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace phuongtrinhb1.Controllers
 {
-    public class phuongtrinhb1Controller : Controller
+    public class phuongtrinhb1Controller 
     {
         // GET: phuongtrinhb1
         public ActionResult phuongtrinhb1()
@@ -14,16 +14,17 @@ namespace phuongtrinhb1.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Timx( string soA , string soB)
+        public ActionResult Timx(string soA, string soB)
         {
+            double so1 = Convert.ToDouble;
+            double so2 = Convert.ToDouble;
+            double gtx = so2 / so1;
+            ViewBag.timgtx = gtx;
             double so1 = Convert.ToDouble(soA);
             double so2 = Convert.ToDouble(soB);
-            double GiaTrix = - so1 / so2;
-            ViewBag.Timgtx=GiaTrix;
+            double gt_x = -so2 / so1;
+            ViewBag.timgtx = gt_x;
             return View();
         }
-
-        
-        
     }
 }
--- Models/ptbac1.cs
+++ Models/ptbac1.cs
@@ -5,23 +5,11 @@ using System.Web;
 
 namespace phuongtrinhb1.Models
 {
-    public class ptbac1
+    public double Timx(double a, double b)
     {
-        public double Timx( double a, double b )
-        {
-            double giattrix = 0;
-            giattrix = - a / b;
-            return giattrix;
-
-
-
-
-        }
-        public double GPT(double x, double y)
-        {
-            double GPT = 0;
-            GPT = -x / y;
-            GPT = -y / x;
-            return GPT;
-        }
+        double gtx = 0;
+        gtx = b / a;
+        gtx = -b / a;
+        return gtx;
+    }
 }
